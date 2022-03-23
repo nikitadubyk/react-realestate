f7ff650b7a7c557963601333e788402a95120db2
Nikita <nikita_dubyk@mail.ru>
2022-03-23 10:37:50 +0300

    fixed premiumAdsSlice

--- src/components/premium-ads/premiumAdsSlice.js
+++ src/components/premium-ads/premiumAdsSlice.js
@@ -1,60 +1,45 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 const initialState = {
     apartaments: [],
     topAdsApartaments: [],
     apartamentLoadingStatus: 'idle',
-};
+}
 
 export const fetchApartaments = createAsyncThunk(
     'apartaments/fetchApartaments',
     async () => {
-        const response = await axios.get('http://localhost:3001/apartaments');
-        return response.data;
+        const response = await axios.get('http://localhost:3001/apartaments')
+        return response.data
     }
-);
+)
 
 const premiumAdsSlice = createSlice({
     name: 'apartaments',
     initialState,
-    reducers: {
-        apartamentsFetching: state => {
-            state.apartamentLoadingStatus = 'loading';
-        },
-        apartamentsFetched: (state, action) => {
-            state.apartamentLoadingStatus = 'idle';
-            state.apartaments = action.payload;
-            state.topAdsApartaments = action.payload.filter(
-                item => item.topAds
-            );
-        },
-        apartamentsFetchingError: state => {
-            state.apartamentLoadingStatus = 'error';
-        },
-    },
     extraReducers: builder => {
         builder
             .addCase(fetchApartaments.pending, state => {
-                state.apartamentLoadingStatus = 'loading';
+                state.apartamentLoadingStatus = 'loading'
             })
             .addCase(fetchApartaments.fulfilled, (state, action) => {
-                state.apartamentLoadingStatus = 'idle';
-                state.apartaments = action.payload;
+                state.apartamentLoadingStatus = 'idle'
+                state.apartaments = action.payload
                 state.topAdsApartaments = action.payload.filter(
                     item => item.topAds
-                );
+                )
             })
             .addCase(fetchApartaments.rejected, state => {
-                state.apartamentLoadingStatus = 'error';
+                state.apartamentLoadingStatus = 'error'
             })
-            .addDefaultCase(() => {});
+            .addDefaultCase(() => {})
     },
-});
+})
 
 export const {
     apartamentsFetching,
     apartamentsFetched,
     apartamentsFetchingError,
-} = premiumAdsSlice.actions;
-export default premiumAdsSlice.reducer;
+} = premiumAdsSlice.actions
+export default premiumAdsSlice.reducer
